68375b4539dec3e91303c33a1ef699eb2f7207f4
khairul-islam98 <janeyrahman900@gmail.com>
2024-09-06 15:12:28 +0600

    create workspaceModal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 
 import { useGetWorkspaces } from "@/features/workspaces/api/use-get-workspaces";
-import { UserButton } from "../features/auth/components/user-button";
+import { UserButton } from "@/features/auth/components/user-button";
 import { useEffect, useMemo } from "react";
+import { useCreateWorkspaceModal } from "@/features/store/use-create-workspace-modal";
 
 export default function Home() {
   const { data, isLoading } = useGetWorkspaces();
+  const [open, setOpen] = useCreateWorkspaceModal()
 
   const workspacesId = useMemo(() => data?.[0]?._id, [data]);
 
