9c5b0e4d51ea95a3e928ba408ae245faab5daffe
khairul-islam98 <janeyrahman900@gmail.com>
2024-09-21 00:30:24 +0600

    renderer file timestamp add

--- src/components/message.tsx
+++ src/components/message.tsx
@@ -1,4 +1,10 @@
+import { format, isToday, isYesterday } from "date-fns";
 import { Doc, Id } from "../../convex/_generated/dataModel";
+
+import dynamic from "next/dynamic";
+import { Hint } from "./hint";
+
+const Renderer = dynamic(()=> import("@/components/renderer"), {ssr: false})
 
 interface MessageProps { 
     id: Id<"messages">
@@ -24,6 +30,9 @@ interface MessageProps {
     threadTimestamp?: number;
 }
 
+const formatFullTime = (date:Date)=> {
+    return `${isToday(date) ? "Today" : isYesterday(date) ? "Yesterday" : format(date, "MMM d, yyyy")} at ${format(date, "h:mm:ss a")}`
+}
 
 
 export const Message = ({
@@ -47,8 +56,15 @@ export const Message = ({
 
 }:MessageProps) => {
     return (
-        <div>
-            {JSON.stringify(body)}
+        <div className="flex flex-col gap-2 px-5 p-1.5 hover:bg-gray-100/60 group relative">
+            <div className="flex items-start gap-2">
+                <Hint label={formatFullTime(new Date(createdAt))}>
+                <button className="text-xs text-muted-foreground opacity-0 group-hover:opacity-100 w-[40px] leading-[22px] text-center hover:underline">
+                    {format(new Date(createdAt), "hh:mm")}
+                </button>
+                </Hint>
+            </div>
+           <Renderer value={body} />
         </div>
     );
 };
