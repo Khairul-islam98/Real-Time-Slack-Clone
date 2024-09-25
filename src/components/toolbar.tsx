3521dd595b93b8959271f186aaa5a4562aa5978e
khairul-islam98 <janeyrahman900@gmail.com>
2024-09-25 13:47:32 +0600

    create message toolbar

---
+++ src/components/toolbar.tsx
@@ -0,0 +1,24 @@
+interface ToolbarProps{
+    isAuthor: boolean;
+    isPending: boolean;
+    handleEdit: ()=> void;
+    handleThread: () => void;
+    handleDelete: () => void;
+    handleReaction: (value: string) => void;
+    hideThreadButton?: boolean
+}
+
+
+export const Toolbar = ({
+    isAuthor,
+    isPending,
+    handleEdit,
+    handleThread,
+    handleDelete,
+    handleReaction,
+    hideThreadButton
+}: ToolbarProps) => {
+    return (
+        <div className=""></div>
+    )
+}
